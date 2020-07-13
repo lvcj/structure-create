7e19a2d118138f772a168591a4c2799dba6ce107
吕春静 <lvchunj@chanjet.com>
2020-07-13 09:30:26 +0800

    其他信息配置

---
+++ index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom"
+import React from "react"
+import Entry from "~/pages/entry"
+import './src/style/css/common.css'
+
+ReactDOM.render(<Entry />, document.getElementById("root"))
